355090727540c477982a9f6600d1defeb2b7ff83
joserobjr <jose.rob.jr@gmail.com>
2019-04-28 18:00:22 -0300

    EP 1: Configurando o ambiente

---
+++ src/main/kotlin/com/youtube/programandominecraft/globalmarketplace/GlobalMarketPlacePlugin.kt
@@ -0,0 +1,10 @@
+package com.youtube.programandominecraft.globalmarketplace
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class GlobalMarketPlacePlugin: JavaPlugin() {
+    override fun onEnable() {
+        logger.info("Está sendo iniciado!")
+    }
+}
